b8cd3ff20eab58d20c65326bed9bc3bbcd511f1c
SteVioli <stefano.violi87@gmail.com>
2022-11-09 11:34:48 +0100

    ste

--- m1/w3/d2/d2esercizio/D7.js
+++ m1/w3/d2/d2esercizio/D7.js
@@ -31,37 +31,103 @@
 /* ESERCIZIO 3
   Scrivi una funzione per creare un array di soli valori multipli di 5, da 1 a 100.
  */
+/*let arr = [];
+for(let i = 0 ; i < 101 ; i++){
+  if( i % 5 === 0)(
+    arr.push(i)
+  );
+}
+console.log(arr)*/
+
 
 /* ESERCIZIO 4
   Scrivi una funzione per creare un array di 10 elementi; ognuno di essi deve essere un valore random compreso tra 0 e 100 (incluso).
  */
 
+ /* let arr = [];
+  for(let i = 0 ; i < 10 ; i++){
+    arr.push(Math.round(Math.random()*100))// valore compreso tra 0 e 100
+  }
+  console.log(arr)*/
+
 /* ESERCIZIO 5
   Scrivi una funzione per ricavare solamente i valori PARI da un array composto da soli valori numerici.
  */
+
+ /* const arr2 = function(arr){
+    let evenArray = []
+    for (let i = 0; i < arr.length ; i++){
+      if(arr[i] % 2 === 0){
+        evenArray.push(arr[i])
+      }
+    }
+    console.log(evenArray)
+  }
+  let array = [34 , 67 ,85 ,35 ,100 , 4];
+  arr2(array)*/
+  
+   
 
 /* ESERCIZIO 6
   Scrivi una funzione per sommare a catena i numeri contenuti in un array.
  */
 
+  
+
+
 /* ESERCIZIO 7
   Scrivi una funzione per incrementare di 1 tutti i valori numerici in un array.
 */
+
+/*function incrementArray (arrayDaIncrementare){
+  for(let i = 0; i < arrayDaIncrementare.length; i++) {
+    arrayDaIncrementare [i]++;
+  }
+  return arrayDaIncrementare;
+}
+console.log(incrementaArray(array))*/
 
 /* ESERCIZIO 8
   Sostituisci ogni stringa contenuta in un array con un numero rappresentante la sua lunghezza.
   es.: ["EPICODE", "is", "great"] => [7, 2, 5]
 */
+/*let animali = ["Adinolfi", "Berlu", "Salvini"];
+function strToNumArray(array) {
+  let s = [];
+  for (let u of array) {
+    s.push(u.length);
+  }
+  return s;
+}
+
+console.log(strToNumArray(animali))*/
 
 /* ESERCIZIO 9 (EXTRA)
   Scrivi una funzione per eliminare solo i valori PARI da un array.
 */
+/*var lista5 = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+const deleteEven = function (arr) {
+
+  let counter = 0;
+  while (counter !== arr.length){//counter diverso dalla lunghezza dell'array
+    if(arr[counter] % 2 === 0){
+      arr.splice(counter , 1)
+    } 
+
+    counter++
+
+  }
+  return arr;
+}
+console.log(deleteEven(lista5))*/
+
+
 
 /* ESERCIZIO 10 (EXTRA)
   Scrivi una funzione per creare un array di 10 elementi; ognuno di essi deve essere un valore random compreso tra 0 e 10 (incluso), SENZA AMMETTERE DUPLICATI.
  */
 
-  const createRandomWithoutDuplicate = function (){ // funzione dichiarata
+  /*const createRandomWithoutDuplicate = function (){ // funzione dichiarata
 
     let array = []                                                                  // creato array vuoto
     
@@ -82,13 +148,41 @@
     
     } 
     
-    console.log(createRandomWithoutDuplicate())
+    console.log(createRandomWithoutDuplicate())*/
+
+    /*function arrayRandom(){ //creo la funzione arrayRandom che non richiede parametri in ingresso 
+      let array = []; //creo un array vuoto chiamato array
+      let numeroRandom = 0;  //creo la variabile numeroRandom nella quale verrà memorizzato un numero random (più avanti nel codice)
+      let i = 0; // creo l'indice i utile per il ciclo while utile per il passagio successivo
+      while( i < 10){ //apro il ciclo while e faccio ciclare fin che l'indice non sarà superiore a 10 (in questo caso quindi una volta chiuso il ciclo creerà un array di 10 elementi)
+        numeroRandom = Math.floor(Math.random() * 11); //alla variabile numeroRandom affindo un numero random da 0 -10 incluso 
+        if(!array.includes(numeroRandom)){ //avvio un if dove diro se in tutto l'array non esiste il valore presente nella variabile numeroRandom allora verifica la condizione interna
+          array.push(numeroRandom); //se la condizione sara verificata nell if pusho nel nostro array la variabile numeroRandom
+          i++; //aumento il contatore del while (in questo caso solo se la condizione del if sarà verificata)
+        }
+    
+      }
+      return array;//ritorno l'array
+    
+    }
+    
+    let array = arrayRandom()
+    
+    console.log(array);*/
+
+
 
 
 /* ESERCIZIO 11 (EXTRA)
   Scrivi un algoritmo in grado di invertire un array.
   es: [1, 3, 5] ==> [5, 3, 1]
 */
+
+let arrayDaInvertire = [1,2,3]
+function inversione(array){
+  return array.reverse()
+}
+console.log(inversione(arrayDaInvertire))
 
 /* Questo array di film verrà usato negli esercizi a seguire. Non modificarlo e scorri oltre per riprendere gli esercizi :) */
 const movies = [
