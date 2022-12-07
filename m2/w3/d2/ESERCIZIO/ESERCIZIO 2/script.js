c802d2d1033ac28c3df94c15b60ae003b665dab2
SteVioli <stefano.violi87@gmail.com>
2022-12-07 13:55:16 +0100

    teoria ajax api json

--- m2/w3/d2/ESERCIZIO/ESERCIZIO 2/script.js
+++ m2/w3/d2/ESERCIZIO/ESERCIZIO 2/script.js
@@ -1,46 +1,84 @@
-let primo = document.getElementById('primo');
-let precedente = document.getElementById('precedente');
-let successivo = document.getElementById('successivo');
-let ultimo = document.getElementById('ultimo');
-
-let items = [
-            { id: 1, nome: 'Luca', cognome: 'Rossi', classe: 'A' },
-            { id: 2, nome: 'Luigi', cognome: 'Verdi', classe: 'A' },
-            { id: 3, nome: 'Mario', cognome: 'Bianchi', classe: 'A' },
-            { id: 4, nome: 'Piero', cognome: 'Neri', classe: 'A' },
-            { id: 5, nome: 'Paolo', cognome: 'Rossi', classe: 'A' },
-            { id: 6, nome: 'Sergio', cognome: 'Verdi', classe: 'A' },
-            { id: 7, nome: 'Gianno', cognome: 'Bianchi', classe: 'A' },
-            { id: 8, nome: 'Davide', cognome: 'Neri', classe: 'A' },
-        ]
-
-class Pagina{
-    constructor(id,nome,cognome,classe){
-        this.id = id;
-        this.nome = nome;
-        this.cognome = cognome;
-        this.classe = classe;
+    //Creare una classe per gestire il contenuto paginato in un sito web.
+    //La paginazione viene utilizzata per dividere lunghi elenchi di contenuti in una serie di pagine.La classe di paginazione accetterà 2 parametri:
+    //items = []
+    //pageSize = 10;
+    
+    class Pagination { // dichiaro la classe Pagination
+        constructor (items = [], pageSize = 10) { //dichiaro il mio costruttore a cui do i valori items, che è un array vuoto , e pageSize al quale darò un numero come valore a seconda di quanto grande voglio la mia sezione della pagina
+            this.items = items  //riporto i vari this dei valori all'interno della pagina
+            this.pageSize = Math.floor(pageSize) //qua dico voglio il numero arrotondato che si avvicina di più a pageSize
+            this.totalPages = Math.ceil(items.length / this.pageSize) || 1 //dichiaro che totalPages equivale al numero arrotondato per difetto dato dalla lunghezza dell array items che ho dichiarto diviso il this.pageSize oppure 1
+            this.currentPage = 1 //dichiaro un valore di currentPage = 1
+        }
+        //applico i metodi che mi serviranno poi da far richiamare agli eventListener
+        prevPage () { //col metodo prevPage dico che currentPage deve essere uguale al valore di current
+            this.currentPage = this.currentPage > 1 ? this.currentPage - 1 : this.currentPage
+            return this
+        }
+        nextPage () {
+            this.currentPage = (this.currentPage + 1 <= this.totalPages) ? this.currentPage + 1 : this.currentPage
+            return this
+        }
+        firstPage () {
+            this.currentPage = 1
+            return this
+        }
+        lastPage () {
+            this.currentPage = this.totalPages
+            return this
+        }
+        goToPage (page) {
+            if (Math.sign(page) === -1) {
+                this.currentPage = 1
+                return this
+            } else {
+                this.currentPage = page > this.totalPages ? this.totalPages : Math.floor(page)
+                return this
+            }
+        }
+        getVisibleItems () {
+            const start = this.currentPage * this.pageSize - this.pageSize;
+            const end = this.currentPage * this.pageSize;
+            //return this.items.slice(start, end);
+            const lista = this.items.slice(start, end);
+            const wrapper = document.querySelector('#wrapper');
+            let strHtml = '';
+            lista.forEach((item)=>{//ciclo di lista che parte dall'inizio di start e finisce con l'inizio di end
+                strHtml += `<tr><td>${item.id}</td><td>${item.nome}</td><td>${item.cognome}</td><td>${item.classe}</td></tr>`;
+            });
+            wrapper.innerHTML = strHtml;
+        }
     }
-    compilaRiga(){
-        let corpo = document.querySelector('tbody');
-        corpo.innerHTML = `<tr><td>${items[0].id}</td><td>${items[0].nome}</td><td>${items[0].cognome}</td><td>${items[0].classe}</td></tr>`
-    }
-}
+    const users = [ //dichiaro una costante di nome users che è un array con 8 oggetti al suo interno
+        {id: 1, nome: 'Luca', cognome: 'Rossi', classe: 'A'},
+        {id: 2, nome: 'Luigi', cognome: 'Verdi', classe: 'A'},
+        {id: 3, nome: 'Mario', cognome: 'Bianchi', classe: 'A'},
+        {id: 4, nome: 'Piero', cognome: 'Neri', classe: 'A'},
+        {id: 5, nome: 'Paolo', cognome: 'Rossi', classe: 'A'},
+        {id: 6, nome: 'Sergio', cognome: 'Verdi', classe: 'A'},
+        {id: 7, nome: 'Gianno', cognome: 'Bianchi', classe: 'A'},
+        {id: 8, nome: 'Davide', cognome: 'Neri', classe: 'A'},
+    ];
+    p = new Pagination(users, 2);
+    p.getVisibleItems();
+    const primo = document.querySelector('#primo');
+    primo.addEventListener('click', function(){
+        p.firstPage();
+        p.getVisibleItems();
+    });
+    const ultimo = document.querySelector('#ultimo');
+    ultimo.addEventListener('click', function(){
+        p.lastPage();
+        p.getVisibleItems();
+    });
+    const precedente = document.querySelector('#precedente');
+    precedente.addEventListener('click', function(){
+        p.prevPage();
+        p.getVisibleItems();
+    });
+    const successivo = document.querySelector('#successivo');
+    successivo.addEventListener('click', function(){
+        p.nextPage();
+        p.getVisibleItems();
+    });
 
-let riga1 = new Pagina(items[0]);
-let riga2 = new Pagina(items[1]);
-let riga3 = new Pagina(items[2]);
-let riga4 = new Pagina(items[3]);
-let riga5 = new Pagina(items[4]);
-let riga6 = new Pagina(items[5]);
-let riga7 = new Pagina(items[6]);
-let riga8 = new Pagina(items[7]);
-
-riga1.compilaRiga()
-
-console.log(riga1);
-
-primo.addEventListener('click',()=>{})
-precedente.addEventListener('click',()=>{})
-successivo.addEventListener('click',()=>{})
-ultimo.addEventListener('click',()=>{})
