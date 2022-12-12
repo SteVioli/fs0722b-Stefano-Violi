92842c9edafb5747aaae4080fe4137e67843d22e
SteVioli <stefano.violi87@gmail.com>
2022-12-12 12:04:55 +0100

    SteVioliFixedTest

--- m2/w3/d5/esercizio/script.js
+++ m2/w3/d5/esercizio/script.js
@@ -2,7 +2,7 @@ let blocco_contenitore = document.querySelector('.container');
 //dichiarato una variabile che tiene conto del "contenitore nel quale far comparire i miei files"
 //console.log(blocco_contenitore);
 
-fetch("../esercizio/JS-Json/users.json")  //richiamo il fetch sull url (in questo caso un file json in locale)
+fetch("users.json")  //richiamo il fetch sull url (in questo caso un file json in locale)
     .then(response => response.json()) // chiamata 
     .then(result => {
         result.forEach(el => {  //ciclo for each , dove richiedo [sempre partendo dal punto di partenza che è richiamare la mia fonte] che per ogni indice dell'array ottenuto, voglio "pescare l'immagine , il nome e il cognome associato"
