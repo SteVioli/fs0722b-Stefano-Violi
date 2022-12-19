09ba3686908e91e16236c8bba35ac7fa938207bf
SteVioli <stefano.violi87@gmail.com>
2022-12-19 16:57:35 +0100

    TypeScript EsD1

--- m3/w1/d1/esercizio/esercizio.js
+++ m3/w1/d1/esercizio/esercizio.js
@@ -13,28 +13,33 @@ function Check(n1, n2) {
     }
     else {
         if (diff_1 < diff_2) {
-            console.log("il primo utente si è avvicinato con il numero: " + n1 + " rispetto al primo utente che ha dichiarato " + n2 + " perchè ha uno scarto di " + diff_1);
+            console.log("il primo utente con il numero: " + n1 + " si è avvicinato rispetto al secondo utente che ha dichiarato " + n2 + " perchè ha uno scarto di " + diff_1);
         }
         else {
-            console.log("il secondo utente si è avvicinato con il numero: " + n2 + " rispetto al secondo utente che ha dichiarato " + n1 + " perchè ha uno scarto di " + diff_2);
+            console.log("il secondo utente con il numero: " + n2 + " si è avvicinato rispetto al primo utente che ha dichiarato " + n1 + " perchè ha uno scarto di " + diff_2);
         }
     }
 }
 Check(5, 8);
 function lunghezzaStringa(s1, s2) {
-    console.log(s1.length);
-    console.log(s2.length);
-    var diffString = Math.abs(s1.length - s2.length);
-    if (s1.length == s2.length) {
-        console.log("stesso numero di caratteri");
+    if (s1 == "" || s2 == "") {
+        console.log("metti dei valori!!!!");
     }
     else {
-        if (s1.length < s2.length) {
-            console.log("la prima parola è più corta della seconda per un numero di " + diffString + " caratteri");
+        console.log(s1.length);
+        console.log(s2.length);
+        var diffString = Math.abs(s1.length - s2.length);
+        if (s1.length == s2.length) {
+            console.log("stesso numero di caratteri");
         }
         else {
-            console.log("la seconda parola è più corta della prima di " + diffString + " caratteri");
+            if (s1.length < s2.length) {
+                console.log("la prima parola è più corta della seconda per un numero di " + diffString + " caratteri");
+            }
+            else {
+                console.log("la seconda parola è più corta della prima di " + diffString + " caratteri");
+            }
         }
     }
 }
-lunghezzaStringa("giulio", "bobo");
+lunghezzaStringa("", "bobo");
